a2e0b3597dc8ee3f6c332462e05bcea6fb9a297a
gilberthedison <hedison_gilbert@live.com>
2018-03-20 14:23:28 +0700

    Update Anagram.java
    salah tulis keterangan problem

--- Anagram.java
+++ Anagram.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
- * problem 5
+ * problem 6
  */
 public class Anagram {
     public static void main(String[] args) throws IOException {
